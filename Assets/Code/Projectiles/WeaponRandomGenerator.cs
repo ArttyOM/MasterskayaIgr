81aacc19f7ab4e8b0b462f8c90ba40ba66a0d4dc
ArttyOM <levsha.elody@gmail.com>
2024-02-01 00:00:07 +0300

    Конвейер предметов готов

--- Assets/Code/Projectiles/WeaponRandomGenerator.cs
+++ Assets/Code/Projectiles/WeaponRandomGenerator.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 using UniRx;
 using UnityEngine;
 using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 namespace Code.Projectiles
 {
@@ -55,15 +56,51 @@ namespace Code.Projectiles
         private void GenerateWeapon()
         {
             ">>GenerateWeapon".Colored(Color.green);
+            MoveNextWeaponToCurrentPosition();
+
+            ProjectileType typeOfWeapon = GenarateRandomNextWeapon();
+            _nextWeapon = _weaponPools[typeOfWeapon].Rent();
+            _nextWeapon.transform.position = _nextWeaponSpawnPoint.transform.position;
+
+            
+        }
+
+        private ProjectileType GenarateRandomNextWeapon()
+        {
+            int sum = 0;
+
+            List<(int, ProjectileType)> spawnChances = new();
+            
+            foreach (var weaponPriorityPair in _weaponSpawnChanceConfig.weaponPriorityPairs)
+            {
+                sum += weaponPriorityPair.priority;
+                if (weaponPriorityPair.priority > 0)
+                {
+                    spawnChances.Add((sum, weaponPriorityPair.weaponPrefab.GetProjectileType));
+                }
+            }
+
+            int random = Random.Range(0, sum);
+            foreach (var spawnChance in spawnChances)
+            {
+                if (spawnChance.Item1 > random) return spawnChance.Item2;
+            }
+            
+            "В конфиге все приоритеты спауна нулевые".Colored(Color.red).LogError();
+            return ProjectileType.Beaver;
+        }
+
+        private void MoveNextWeaponToCurrentPosition()
+        {
             if (_nextWeapon != null)
             {
+                if (_currentWeapon != null)
+                {
+                    _weaponPools[_currentWeapon.GetProjectileType].Return(_currentWeapon);
+                }
                 _currentWeapon = _nextWeapon;
                 _currentWeapon.transform.DOMove(_currentWeaponSpawnPoint.transform.position, 0.4f);
             }
-
-            _nextWeapon = _weaponPools[ProjectileType.Beaver].Rent();
-            _nextWeapon.transform.position = _nextWeaponSpawnPoint.transform.position;
-
         }
     }
 }
