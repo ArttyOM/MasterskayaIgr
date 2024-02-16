b5349e1140352308d941ec69c4a7d0ff6b34214f
Artem Smirnov <tonakai.personal@gmail.com>
2024-02-16 13:16:25 +0300

    Removed MasterServerToolkit and Example Games

--- Assets/MasterServerToolkit/MasterServer/Scripts/Modules/Profiles/ProfilePropertyOpCodes.cs
+++
@@ -1,6 +0,0 @@
-namespace MasterServerToolkit.MasterServer
-{
-    public partial struct ProfilePropertyOpCodes
-    {
-    }
-}
--- Assets/MasterServerToolkit/Tools/UI/Scripts/Views/InputHandlers/IUIViewInputHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MasterServerToolkit.UI
-{
-    public interface IUIViewInputHandler
-    {
-    }
-}
